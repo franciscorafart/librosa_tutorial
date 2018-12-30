c3049f5444683c77f4ecb4978812500fe8c0f404
franciscorafart <francisco.rafart@gmail.com>
2018-12-30 14:05:57 -0500

    display audio

--- audio.py
+++ audio.py
@@ -1,6 +1,18 @@
 import librosa
+import IPython.display as ipd
+import matplotlib.pyplot as plt
 
+#Opening audio file
 audio_path = './techno.wav'
 x, sr = librosa.load(audio_path, sr=44100)
-
 print(x.shape, sr)
+
+#playing sound file
+ipd.Audio(audio_path)
+
+plt.figure(figsize=(14, 5))
+librosa.display.waveplot(x, sr=sr)
+
+print('Reaches the end')
+
+
